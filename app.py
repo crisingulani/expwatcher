2178df34f609d94b59e77da6fd924d7973b26e8f
Cristiano Singulani <crisingulani@gmail.com>
2019-05-01 00:25:13 -0300

    Improvements:  - Added method 'run' in Thread with possibility to stop it.  - Added logic to check if the file size is still increasing (so we guarantee the complete file before we read).  - Improved logging

--- app.py
+++ app.py
@@ -6,37 +6,45 @@ import logging
 from watchdog.observers import Observer
 from watchdog.events import RegexMatchingEventHandler
 
-logging.basicConfig(
-    filename='expwatcher.log', filemode='w',
-    format='%(name)s - %(levelname)s - %(message)s'
-)
-
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler('exp-watcher.log')
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '%(asctime)s: %(name)s ~ %(levelname)s: %(message)s'
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
-class ExpWatcher:
+class ExposureWatcher:
     def __init__(self, src_path):
         self.__src_path = src_path
-        self.__event_handler = ExpEventHandler()
+        self.__event_handler = ExposureHandler()
         self.__event_observer = Observer()
 
     def run(self):
         self.start()
         try:
-            while True:
+            while True: 
+                global stop_thread 
+                if stop_thread: 
+                    break
                 time.sleep(1)
-        except KeyboardInterrupt:
+        except Exception:
+            logger.exception("File watcher interrupted")
+        finally:
             self.stop()
 
     def start(self):
         self.__schedule()
         self.__event_observer.start()
-        logger.info('Exposure Watcher started!')
+        logger.info('File watcher started!')
 
     def stop(self):
         self.__event_observer.stop()
-        logger.info('Exposure Watcher stoped!')
         self.__event_observer.join()
+        logger.info('File watcher stoped!')
 
     def __schedule(self):
         self.__event_observer.schedule(
@@ -46,21 +54,35 @@ class ExpWatcher:
         )
 
 
-class ExpEventHandler(RegexMatchingEventHandler):
-    # EXP_REGEX = [r".*[^_thumbnail]\.jpg$"]
-    EXP_REGEX = [r".*"]
+class ExposureHandler(RegexMatchingEventHandler):
+    # FILE_REGEX = [r".*\.fits$"]
+    FILE_REGEX = [r".*"]
 
     def __init__(self):
-        super().__init__(self.EXP_REGEX)
+        super().__init__(self.FILE_REGEX)
 
     def on_created(self, event):
+        file_size = -1
+        while file_size != os.path.getsize(event.src_path):
+            file_size = os.path.getsize(event.src_path)
+            time.sleep(2)
+
         self.process(event)
 
     def process(self, event):
-        filename, ext = os.path.splitext(event.src_path)
-        logger.info("Welcome {} file! :)".format(filename))
+        filename = os.path.basename(event.src_path)
+        logger.info("Welcome {} exposure! :)".format(filename))
 
  
 if __name__ == "__main__":
+    import threading  
     src_path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    ExpWatcher(src_path).run()
+    exp = ExposureWatcher(src_path)
+    stop_thread = False
+    t1 = threading.Thread(target=exp.run) 
+    t1.start() 
+    # simulate exposures arrival: it is time to copy the test
+    # exposures in the 'src_path' directory.
+    time.sleep(5) 
+    stop_thread = True
+    print('Bye')
